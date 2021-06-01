32b81ec18e52fb1710a92212a78bb150a9a39691
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2021-06-01 22:18:54 +0530

    bootstrap and grid layout files added

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,3 @@
-console.log("Hello World");
+import "../scss/style.scss";
+
 document.getElementsByTagName("body")[0].style.backgroundColor = "red";
--- webpack.config.js
+++ webpack.config.js
@@ -26,4 +26,19 @@ module.exports = {
       });
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.scss/,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
 };
