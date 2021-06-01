9048069ca69dc41eef353d42a9ecdda254d3e293
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2021-06-01 17:55:38 +0530

    Basic  config

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+var path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/js/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist/js"),
+  },
+};
